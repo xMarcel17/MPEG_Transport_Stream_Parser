4e6790dfc088514551cc2bedb63d307ec7e232de
xMarcel17 <marcelradtke17@gmail.com>
2024-04-03 03:08:08 +0200

    Commit4 (Parse+Print+while w main + pojedyncze rzeczy)

--- TS_parser.cpp
+++ TS_parser.cpp
@@ -23,27 +23,31 @@ int main(int argc, char *argv[ ], char *envp[ ])
   }
 
   xTS_PacketHeader TS_PacketHeader;
-  char *TS_PacketBuffer[xTS::TS_PacketLength]; //stworzenie tablicy (bufora) przechowujacej pojedynczy pakiet strumienia transportowego; gdyby nie fakt, ze TS_PacketLength jest const, to nie moglbym tutaj go ustawic jako rozmiar tablicy
+  char *TS_PacketBuffer = new char[xTS::TS_PacketLength]; //stworzenie tablicy dynamicznej (bufora) przechowujacej pojedynczy pakiet strumienia transportowego
 
   int32_t TS_PacketId = 0;
-  while(!file.eof() == true) //petla wykonuje sie dopoki nie zostanie osiagniety koniec pliku
+  while(!file.eof() == true && TS_PacketId < 34) //petla wykonuje sie dopoki nie zostanie osiagniety koniec pliku
   {
     // TODO - read from file (done7)
-    file.read(*TS_PacketBuffer, xTS::TS_PacketLength); //read() zczytuje pierwsze 188B do bufora i w kolejnej iteracji zczytuje kolejne 188B itd.
-    
-    TS_PacketHeader.Reset();
-    TS_PacketHeader.Parse(/*TS_PacketBuffer*/ nullptr);
+    file.read(TS_PacketBuffer, xTS::TS_PacketLength); //read() zczytuje pierwsze 188B do bufora i w kolejnej iteracji zczytuje kolejne 188B itd.
+    uint8_t *TS_PacketBufferFinal = reinterpret_cast<uint8_t*>(TS_PacketBuffer); //rzutowanie typu tablicy na uint8_t (z racji iz nasz Input w Parse() to uint8_t, wiec musimy to wykonac) 
 
-    printf("%010d ", TS_PacketId);
-    TS_PacketHeader.Print();
-    printf("\n");
+    if(TS_PacketBuffer[0] == 71){ //upewnienie sie, ze poczatek pakietu (pierwsze 8 bitow - Sync Byte) jest rowny 0x47)
+      TS_PacketHeader.Reset();
+      TS_PacketHeader.Parse(TS_PacketBufferFinal);
 
-    TS_PacketId++;
+      printf("%010d ", TS_PacketId);
+      TS_PacketHeader.Print();
+      printf("\n");
+
+      TS_PacketId++;
+    }
   }
   
   // TODO - close file (done8)
   file.close();
 
+  delete[] TS_PacketBuffer; //zwolnienie zaalokowanej pamieci (tablicy) po jej uzyciu, by uniknac wyciekow pamieci
   return EXIT_SUCCESS;
 }
 
--- tsTransportStream.cpp
+++ tsTransportStream.cpp
@@ -1,4 +1,5 @@
 #include "tsTransportStream.h"
+#include <iostream>
 
 //=============================================================================================================================================================================
 // xTS_PacketHeader
@@ -25,14 +26,105 @@ void xTS_PacketHeader::Reset()
  */
 int32_t xTS_PacketHeader::Parse(const uint8_t* Input)
 {
-  //i`m empty
+  //i`m empty (done9)
+  m_SB = Input[0];
+
+  uint8_t mask = 0b10000000;
+  if((mask & Input[1]) > 0){
+    m_E = 1;
+  } else m_E = 0;
+
+  mask = 0b01000000;
+  if((mask & Input[1]) > 0){
+    m_S = 1;
+  } else m_S = 0;
+
+  mask = 0b00100000;
+  if((mask & Input[1]) > 0){
+    m_T = 1;
+  } else m_T = 0;
+
+  //m_PID
+  uint16_t m_PID_value = 0;
+
+  mask = 0b00010000;
+  if((mask & Input[1]) > 0){
+    m_PID_value += 4096;
+  } else m_PID_value += 0; 
+
+  mask = 0b00001000;
+  if((mask & Input[1]) > 0){
+    m_PID_value += 2048;
+  } else m_PID_value += 0; 
+
+  mask = 0b00000100;
+  if((mask & Input[1]) > 0){
+    m_PID_value += 1024;
+  } else m_PID_value += 0; 
+
+  mask = 0b00000010;
+  if((mask & Input[1]) > 0){
+    m_PID_value += 512;
+  } else m_PID_value += 0;
+
+  mask = 0b00000001;
+  if((mask & Input[1]) > 0){
+    m_PID_value += 256;
+  } else m_PID_value += 0;
+ 
+  m_PID = m_PID_value + ((uint16_t)Input[2]);
+
+  //m_TSC 
+  mask = 0b10000000; 
+  if((mask & Input[3]) > 0){
+    m_TSC += 2;
+  } else m_TSC += 0;
+
+  mask = 0b01000000; 
+  if((mask & Input[3]) > 0){
+    m_TSC += 1;
+  } else m_TSC += 0;
+
+  //m_AFC
+  mask = 0b00100000; 
+  if((mask & Input[3]) > 0){
+    m_AFC += 2;
+  } else m_AFC += 0;
+
+  mask = 0b00010000; 
+  if((mask & Input[3]) > 0){
+    m_AFC += 1;
+  } else m_AFC += 0;
+
+  //m_CC
+  mask = 0b00001000; 
+  if((mask & Input[3]) > 0){
+    m_CC += 8;
+  } else m_CC += 0;
+
+  mask = 0b00000100; 
+  if((mask & Input[3]) > 0){
+    m_CC += 4;
+  } else m_CC += 0;
+
+  mask = 0b00000010; 
+  if((mask & Input[3]) > 0){
+    m_CC += 2;
+  } else m_CC += 0;
+
+  mask = 0b00000001; 
+  if((mask & Input[3]) > 0){
+    m_CC += 1;
+  } else m_CC += 0;
+
   return NOT_VALID;
 }
 
 /// @brief Print all TS packet header fields
 void xTS_PacketHeader::Print() const
 {
-  //print sth
+  //print sth (done10)
+  std::cout << "TS:  SB=" << (int)m_SB << "  E=" << (int)m_E << " S=" << (int)m_S << " T=" << (int)m_T << " PID=" << (int)m_PID << " TSC=" << (int)m_TSC << " AF=" << (int)m_AFC << "  CC=" << (int)m_CC << std::endl; 
 }
 
 //=============================================================================================================================================================================
